8bf67e4b4f110e1582a399d728626d8221bffdbf
Nonimus <jpceron0@misena.edu.co>
2022-11-24 10:43:16 -0500

    changes

---
+++ src/controller/MunicipiosController/municipiosController.js
@@ -0,0 +1,77 @@
+const modelMunicipio = require('../../models/Municipio/municipioModel')
+const estructuraApi = require('../../helpers/estructuraApi')
+
+//search all users
+exports.allMunicipio = async (req, res) => {
+    let api = new estructuraApi()
+    const municipio = await modelMunicipio.findAll()
+    if (municipio.length > 0) {
+        api.setResultado(municipio)
+    } else {
+        api.setEstado(404, "nutrientes entry", "list type nutriente empty")
+    }
+    res.json(api.toResponse())
+}
+
+//search user by :id
+exports.viewMunicipio = async (request, response) => {
+    let api = new estructuraApi()
+    const { id_municipio } = request.params
+    const municipio = await modelMunicipio.findOne({ where: { id_municipio } })
+
+    if (municipio != null) {
+        api.setResultado(municipio)
+    } else {
+        api.setEstado('204', 'Empty', "consult success but Empty")
+    }
+    response.json(api.toResponse())
+}
+
+//update One users
+exports.UpdateMunicipio = async (request, response) => {
+    const api = new estructuraApi()
+    const { id_municipio } = request.params
+    const municipio = request.body
+    const update = await modelMunicipio.update(municipio, { where: { id_municipio } })
+
+    if (update[0] > 0) {
+        api.setResultado(municipio)
+    } else {
+        api.setEstado(204, 'Empty', "id usuario no encontrado")
+    }
+
+    response.json(api.toResponse())
+}
+
+//create user
+exports.CreateMunicipio = async (request, response) => {
+    let api = new estructuraApi()//instanciar
+    const municipio = request.body // igualo el body a mi class
+
+    const create = await modelMunicipio.create(municipio)
+        .catch(err => {
+            api.setEstado(err.parent.code|| 402,"error", err.message || err)
+            return response.json(api.toResponse())
+        })
+    api.setResultado(create)
+
+    response.json(api.toResponse())
+}
+
+//Delete user
+exports.DeleteMunicipio = async (request, response) => {
+    let api = new estructuraApi()
+    let { id_municipio } = request.params
+    await modelMunicipio.destroy({where:{id_municipio}})
+        .then(succ => {
+            if (succ != 0) {
+                api.setEstado('SUCC', 'success', `id_usuaio{${id_municipio}}:delete successfully`)
+            } else {
+                api.setEstado('INFO', 'info', `id_usuaio{${id_municipio}}:!NO ENCONTRADO!`)
+            }
+        })
+        .catch(err => {
+            api.setEstado(err.parent.code, "error", err.parent.detail)
+        })
+    response.json(api.toResponse())
+}
--- src/router/routes_index.js
+++ src/router/routes_index.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const alimentosController = require("../controller/AlimentosController/alimentosController")
 const loginController = require("../controller/AutenticacionController/loginController")
 const reqAnimalesController = require("../controller/RequerimientosAnimalesController/requerimientosAnimalesController")
+const usersControllers = require("../controller/Usuarios/usuariosController")
+const typeNutrienteControllers = require("../controller/TipoNutrientesController/tipoNutrientesController")
+const municipiosController = require("../controller/MunicipiosController/municipiosController")
 
 ///////////////////////////////Routes Alimentos/////////////////////////////////////////
 router.get('/allaliments', alimentosController.allAliments)
@@ -13,13 +16,17 @@ router.delete('/deletealiment/:id_alimentos', alimentosController.deleteAssignme
 router.get('/alimentsite/:region_id', alimentosController.AlimentsForSite)
 // router.get('/alimentestatus/:especie_id', listRequerimientoAcontroller.especieForEstado)
 
-
-
 ///////////////////////////////Routes Requerimiento de Animales/////////////////////////////////////////
 router.get('/all_req_aliments', reqAnimalesController.allReqAliments)
 
+/////////////////////////////// Login /////////////////////////////////////////
 router.post('/login', loginController.LoginUser)
 
+router.get('/allmunicipio', municipiosController.allMunicipio)
+router.get('/viewmunicipio/:id_municipio', municipiosController.viewMunicipio)
+router.post('/updatemunicipio/:id_municipio', municipiosController.UpdateMunicipio)
+router.post('/createmunicipio', municipiosController.CreateMunicipio)
+router.delete('/deletemunicipio/:id_municipio', municipiosController.DeleteMunicipio)
 
 
 module.exports = router;
